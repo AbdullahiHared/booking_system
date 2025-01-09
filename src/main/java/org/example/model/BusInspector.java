b48e515b2604d4fc5361b49512ad4a7cba7e81a8
Abdullah Hared <muxsinxareed6@gmail.com>
2025-01-09 11:02:32 +0100

    Add: getters and setters

--- src/main/java/org/example/model/BusInspector.java
+++ src/main/java/org/example/model/BusInspector.java
@@ -5,17 +5,57 @@ import java.util.List;
 
 public class BusInspector {
     private int inspectorId;
-    private int busNumber;
+    private String busNumber;
     private String busDate;
     private Double currentProfit;
-    private List <String> inspectorLogs;
+    private List<String> inspectorLogs = new ArrayList<>();
 
     // Constructor
-    public BusInspector(int inspectorId, int busNumber, String busDate, Double currentProfit) {
+    public BusInspector(int inspectorId, String busNumber, String busDate, Double currentProfit) {
         this.inspectorId = inspectorId;
         this.busNumber = busNumber;
         this.busDate = busDate;
         this.currentProfit = currentProfit;
-        this.inspectorLogs = new ArrayList<String>();
+    }
+
+    // Getters and Setters
+    public int getInspectorId() {
+        return inspectorId;
+    }
+
+    public void setInspectorId(int inspectorId) {
+        this.inspectorId = inspectorId;
+    }
+
+    public String getBusNumber() {
+        return busNumber;
+    }
+
+    public void setBusNumber(String busNumber) {
+        this.busNumber = busNumber;
+    }
+
+    public String getBusDate() {
+        return busDate;
+    }
+
+    public void setBusDate(String busDate) {
+        this.busDate = busDate;
+    }
+
+    public Double getCurrentProfit() {
+        return currentProfit;
+    }
+
+    public void setCurrentProfit(Double currentProfit) {
+        this.currentProfit = currentProfit;
+    }
+
+    public List<String> getInspectorLogs() {
+        return inspectorLogs;
+    }
+
+    public void addInspectorLog(String log) {
+        this.inspectorLogs.add(log);
     }
 }
