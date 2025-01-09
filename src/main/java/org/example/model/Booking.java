d3a709cd470dd5be2ca482ed83746529c3dbcbe5
Abdullah Hared <muxsinxareed6@gmail.com>
2025-01-09 10:30:56 +0100

    Added a constructor

--- src/main/java/org/example/model/Booking.java
+++ src/main/java/org/example/model/Booking.java
@@ -1,4 +1,20 @@
 package org.example.model;
+import java.util.Date;
 
 public class Booking {
+    private int id;
+    private int passengerId;
+    private String passengerName;
+    private Date bookingDate;
+    private String bookingTime;
+
+    // Constructors
+    public Booking(int id, int passengerId,String passengerName, Date bookingDate, String bookingTime) {
+        this.passengerName = passengerName;
+        this.bookingDate = bookingDate;
+        this.bookingTime = bookingTime;
+        this.passengerId = passengerId;
+        this.id = id;
+    }
+
 }
