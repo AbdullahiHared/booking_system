7b8a5175b8f0336211baf956a6665d7992d310d7
Abdullah Hared <muxsinxareed6@gmail.com>
2025-01-09 10:51:57 +0100

    Refactor: BusInspector constructor

--- src/main/java/org/example/model/BusInspector.java
+++ src/main/java/org/example/model/BusInspector.java
@@ -4,22 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BusInspector {
-    private String inspectorId;
-    private String busNumber;
+    private int inspectorId;
+    private int busNumber;
     private String busDate;
     private Double currentProfit;
     private List <String> inspectorLogs;
 
     // Constructor
-    public BusInspector(String inspectorId, String busNumber, String busDate, Double currentProfit) {
+    public BusInspector(int inspectorId, int busNumber, String busDate, Double currentProfit) {
         this.inspectorId = inspectorId;
         this.busNumber = busNumber;
         this.busDate = busDate;
         this.currentProfit = currentProfit;
         this.inspectorLogs = new ArrayList<String>();
     }
-
-
-
-
 }
