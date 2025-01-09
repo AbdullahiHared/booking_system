be1fb01d7d7bd272ae9663691812a9dbe5935593
Abdullah Hared <muxsinxareed6@gmail.com>
2025-01-09 10:45:54 +0100

    Add BusInspector class to handle inspector operations

--- src/main/java/org/example/model/BusInspector.java
+++ src/main/java/org/example/model/BusInspector.java
@@ -1,4 +1,17 @@
 package org.example.model;
 
+import java.util.List;
+
 public class BusInspector {
+    private String inspectorId;
+    private String busNumber;
+    private String busDate;
+    private Double currentProfit;
+    private List <String> inspectorLogs;
+
+    //
+
+
+
+
 }
