8473bc2debb9b7a01791db1a0e753183badff37d
Abdullah Hared <muxsinxareed6@gmail.com>
2025-01-10 10:17:48 +0100

    Added: variables

--- src/main/java/org/example/model/ProfitManager.java
+++ src/main/java/org/example/model/ProfitManager.java
@@ -1,4 +1,13 @@
 package org.example.model;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProfitManager {
+
+    // variables
+    private double totalPrice;
+    private final double adultTicketPrice = 299;
+    private final double childTicketPrice = 149.50;
+    private List<String> profitLogs;
+
 }
