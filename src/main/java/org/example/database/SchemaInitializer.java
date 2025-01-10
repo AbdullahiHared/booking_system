a064fc8708b368aadfda46d71719faa7161385c1
Abdullah Hared <muxsinxareed6@gmail.com>
2025-01-10 21:47:18 +0100

    Add: seats table

--- src/main/java/org/example/database/SchemaInitializer.java
+++ src/main/java/org/example/database/SchemaInitializer.java
@@ -13,6 +13,16 @@ public class SchemaInitializer {
                         "full_name VARCHAR(100) NOT NULL, " +
                         "birth_date DATE NOT NULL, " ,
 
+                // Create the Seats table
+                "CREATE TABLE IF NOT EXISTS Seats (" +
+                        "seat_id INT AUTO_INCREMENT PRIMARY KEY, " +
+                        "seat_number INT NOT NULL, " +
+                        "is_reserved BOOLEAN DEFAULT FALSE, " +
+                        "passenger_name VARCHAR(100), " +
+                        "passenger_birthdate DATE, " +
+                        "booking_date DATE, " +
+                        "price DECIMAL(10, 2), " +
+                        "FOREIGN KEY (bus_id) REFERENCES Buses(bus_id));",
         };
     }
 }
