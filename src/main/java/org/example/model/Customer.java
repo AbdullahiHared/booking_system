593854ebdbc8ef4c27d50d5cb8ca1e0543012ed6
Abdullah Hared <muxsinxareed6@gmail.com>
2025-01-10 10:08:32 +0100

    Set up getters and setters

--- src/main/java/org/example/model/Customer.java
+++ src/main/java/org/example/model/Customer.java
@@ -1,15 +1,52 @@
 package org.example.model;
 
+import java.time.LocalDate;
+
 public class Customer {
     private int customerId;
     private String firstName;
     private String lastName;
-    private int birthDate;
-
-    public Customer(int customerId, String firstName, String lastName, int birthDate) {
+    private LocalDate birthDate;
+    public Customer(int customerId, String firstName, String lastName, LocalDate birthDate) {
         this.customerId = customerId;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.birthDate = birthDate;
+    }
+
+    // Getter and Setter for customerId
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    // Getter and Setter for firstName
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    // Getter and Setter for lastName
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    // Getter and Setter for birthDate
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
     }
 }
