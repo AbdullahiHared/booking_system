aeec3d2f459f0ce1facc1f09f1a4adb353894626
Abdullah Hared <muxsinxareed6@gmail.com>
2025-01-10 21:44:31 +0100

    Modify: customers table

--- src/main/java/org/example/database/SchemaInitializer.java
+++ src/main/java/org/example/database/SchemaInitializer.java
@@ -9,10 +9,10 @@ public class SchemaInitializer {
         String[] schemaSQL = {
                 // Create the Users table
                 "CREATE TABLE IF NOT EXISTS Customers (" +
-                        "user_id INT AUTO_INCREMENT PRIMARY KEY, " +
-                        "name VARCHAR(100) NOT NULL, " +
-                        "email VARCHAR(100) UNIQUE NOT NULL, " +
-                        "password VARCHAR(255) NOT NULL);",
+                        "customer_id INT AUTO_INCREMENT PRIMARY KEY, " +
+                        "full_name VARCHAR(100) NOT NULL, " +
+                        "birth_date DATE NOT NULL, " ,
+
         };
     }
 }
