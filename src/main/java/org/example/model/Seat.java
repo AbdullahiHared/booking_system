261648e71d400a2b33aa0a033e4e86fe8bebccc0
Abdullah Hared <muxsinxareed6@gmail.com>
2025-01-10 10:35:47 +0100

    Inialize: Variables

--- src/main/java/org/example/model/Seat.java
+++ src/main/java/org/example/model/Seat.java
@@ -1,4 +1,13 @@
 package org.example.model;
 
+import java.time.LocalDate;
+
 public class Seat {
+    private int seatNumber;
+    private boolean isReserved;
+    private String passengerName;
+    private LocalDate passengerBirthDate;
+    private LocalDate bookingDate;
+    private double price;
+
 }
