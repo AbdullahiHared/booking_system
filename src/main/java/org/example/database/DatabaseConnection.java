f0e86a7ab2c71dacbc1ab4c55005b925272d9099
Abdullah Hared <muxsinxareed6@gmail.com>
2025-01-10 21:15:21 +0100

    import sql packages for db connection

--- src/main/java/org/example/database/DatabaseConnection.java
+++ src/main/java/org/example/database/DatabaseConnection.java
@@ -1,4 +1,15 @@
 package org.example.database;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DatabaseConnection {
+    private static String URL = "jdbc:mysql://localhost:3306/booking_system";
+    private static String USER = "root";
+    private static String PASSWORD = "password";
+
+    public static Connection getConnection () {
+
+    }
+
 }
