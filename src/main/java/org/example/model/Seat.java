44b5c0d896271dceb9d8b6b72bbecfe1d31650ba
Abdullah Hared <muxsinxareed6@gmail.com>
2025-01-10 14:12:16 +0100

    Add: methods to getSeatDetails and reserve a seat

--- src/main/java/org/example/model/Seat.java
+++ src/main/java/org/example/model/Seat.java
@@ -1,5 +1,6 @@
 package org.example.model;
 
+import javax.sound.sampled.FloatControl;
 import java.time.LocalDate;
 
 public class Seat {
@@ -20,6 +21,30 @@ public class Seat {
         this.passengerBirthDate = null;
         this.bookingDate = null;
         this.price = price;
+    }
+
+    // Method to return seat details
+    public String getSeatDetails() {
+        return "Seat Number: " + seatNumber +
+                ", Is Reserved: " + isReserved +
+                ", Passenger Name: " + (passengerName != null ? passengerName : "N/A") +
+                ", Passenger Birthdate: " + (passengerBirthDate != null ? passengerBirthDate : "N/A") +
+                ", Booking Date: " + (bookingDate != null ? bookingDate : "N/A") +
+                ", Price: " + price;
+    }
+
+    // Method to reserve seat
+    public void reserveSeat(String passengerName, LocalDate passengerBirthDate, LocalDate bookingDate, double price) {
+        if (isReserved) {
+            System.out.println("Seat is already reserved.");
+        } else {
+            isReserved = true;
+            this.passengerName = passengerName;
+            this.passengerBirthDate = passengerBirthDate;
+            this.bookingDate = bookingDate;
+            this.price = price;
+            System.out.println("Seat reserved successfully");
+        }
     }
 
 
