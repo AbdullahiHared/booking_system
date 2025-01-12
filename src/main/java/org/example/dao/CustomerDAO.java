1e2070eec267836616fc91a27d58e353cbb279c0
Abdullah Hared <muxsinxareed6@gmail.com>
2025-01-12 13:29:09 +0100

    Feat: import needed package classes

--- src/main/java/org/example/dao/CustomerDAO.java
+++ src/main/java/org/example/dao/CustomerDAO.java
@@ -1,4 +1,11 @@
 package org.example.dao;
 
+import org.example.model.Customer;
+
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class CustomerDAO {
 }
