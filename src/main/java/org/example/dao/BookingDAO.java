88800db16d0bce4b8c8b0e1cf1c6c24afdc7303a
Abdullah Hared <muxsinxareed6@gmail.com>
2025-01-12 10:41:52 +0100

    Add: method for getting all methods in the db

--- src/main/java/org/example/dao/BookingDAO.java
+++ src/main/java/org/example/dao/BookingDAO.java
@@ -15,7 +15,7 @@ public class BookingDAO {
 
     // Add a new booking
     public void addBooking(Booking booking) throws SQLException {
-        String sql = "INSERT INTO bookings (passenger_id, booking_name, booking_date, booking_time) VALUES (?, ?, ?, ?)";
+        String sql = "INSERT INTO bookings (passenger_id, passenger_name, booking_date, booking_time) VALUES (?,?, ?, ?)";
         try (PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
             ps.setInt(1, booking.getPassengerId());
             ps.setString(2, booking.getPassengerName());
@@ -39,5 +39,30 @@ public class BookingDAO {
             System.out.println(e.getErrorCode());
             System.out.println(e.getMessage());
         }
+    }
+
+    // Get all bookings
+    public List<Booking> getAllBookings() throws SQLException {
+        List<Booking> bookings = new ArrayList<>();
+        String query = "SELECT * FROM bookings";
+        try (Statement stmt = connection.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
+            while(rs.next()) {
+                Booking booking = new Booking();
+                booking.setId(rs.getInt("Booking_id"));
+                booking.setPassengerId(rs.getInt("passenger_id"));
+                booking.setPassengerName(rs.getString("passenger_name"));
+                booking.setBookingDate(rs.getDate("booking_date"));
+                booking.setBookingTime((rs.getTime("booking_time")).toString());
+
+                bookings.add(booking);
+            }
+        }
+        catch (SQLException e) {
+            System.err.println("Error fetching bookings: " + e.getMessage());
+        }
+
+        return bookings;
+
     }
 }
