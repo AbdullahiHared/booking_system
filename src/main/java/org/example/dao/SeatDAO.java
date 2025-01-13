737ec09287cb71ba16a74726d5c255c534e25270
Abdullah Hared <muxsinxareed6@gmail.com>
2025-01-13 09:17:35 +0100

    Feat: add a method for adding seat into the db

--- src/main/java/org/example/dao/SeatDAO.java
+++ src/main/java/org/example/dao/SeatDAO.java
@@ -2,12 +2,38 @@ package org.example.dao;
 import org.example.model.Seat;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class SeatDAO {
     private Connection connection;
 
     public SeatDAO(Connection connection) {
         this.connection = connection;
+    }
+
+    // Add new seat to the db
+    public void addSeat(Seat seat) {
+        String query = "INSERT INTO seats (seat_number, is_reserved, passenger_name, passenger_birthdate, price) VALUES (?, ?, ?, ?, ?)";
+
+        try (PreparedStatement psmt = connection.prepareStatement(query)) {
+            psmt.setInt(1, seat.getSeatNumber());
+            psmt.setBoolean(2, seat.isReserved());
+            psmt.setString(3, seat.getPassengerName());
+            psmt.setDate(4, seat.getPassengerBirthDate());
+            psmt.setDouble(5, seat.getPrice());
+
+            int rowsAffected = psmt.executeUpdate();
+            if (rowsAffected > 0) {
+                System.out.println("Seat successfully booked");
+            } else {
+                System.out.println("Could not book seat.");
+            }
+
+        } catch (SQLException e) {
+            // log errors
+            System.out.println(e.getMessage());
+        }
     }
 
 }
