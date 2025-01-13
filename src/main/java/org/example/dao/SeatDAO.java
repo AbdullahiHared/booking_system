1879f96586a1c6425d068c3d02211f770ecb98fc
Abdullah Hared <muxsinxareed6@gmail.com>
2025-01-13 08:59:59 +0100

    Feat: Add a constructor

--- src/main/java/org/example/dao/SeatDAO.java
+++ src/main/java/org/example/dao/SeatDAO.java
@@ -1,4 +1,13 @@
 package org.example.dao;
+import org.example.model.Seat;
+
+import java.sql.Connection;
 
 public class SeatDAO {
+    private Connection connection;
+
+    public SeatDAO(Connection connection) {
+        this.connection = connection;
+    }
+
 }
