9a8086c715bea54b5932349e3a52b20f08798a15
Abdullah Hared <muxsinxareed6@gmail.com>
2025-01-16 21:51:50 +0100

    Test: successfully tested getting customer by id

--- src/test/java/CustomerDaoTest.java
+++ src/test/java/CustomerDaoTest.java
@@ -17,23 +17,8 @@ public class CustomerDaoTest {
             // create customerDao instance
             CustomerDAO customerDao = new CustomerDAO(connection);
 
-            // Test: add new user
-            Customer customer = new Customer();
-            String name = "TesName";
-            LocalDate birthDate = LocalDate.of(1904, 3, 11);
-
-            // Test: delete customer by id
-            customerDao.deleteCustomerById(1);
-            customerDao.deleteCustomerById(2);
-            customerDao.deleteCustomerById(3);
-            customerDao.deleteCustomerById(4);
-            customerDao.deleteCustomerById(5);
-            customerDao.deleteCustomerById(6);
-            customerDao.deleteCustomerById(7);
-            customerDao.deleteCustomerById(8);
-            customerDao.deleteCustomerById(9);
-            customerDao.deleteCustomerById(10);
-
+            // test: get customer by id
+            customerDao.getCustomerById(12);
         } catch (SQLException e) {
             System.out.println("Could not add customer into db");
             System.out.println(e.getMessage());
