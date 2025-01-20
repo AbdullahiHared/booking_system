cb517abc86e2c1b9c19784a150eb15efa66c2468
Abdullah Hared <muxsinxareed6@gmail.com>
2025-01-20 05:35:22 +0100

    Successfully tested retrieving booking by id

--- src/test/java/BookingDAOTest.java
+++ src/test/java/BookingDAOTest.java
@@ -20,6 +20,7 @@ public class BookingDAOTest {
 
             System.out.println("=== Testing BookingDAO ===");
 
+            /*
             // 1. Add a new booking
             Booking newBooking = new Booking();
             newBooking.setId(12); //
@@ -29,7 +30,13 @@ public class BookingDAOTest {
                 System.out.println("Booking added with ID: " + newBooking.getId());
             } else {
                 System.out.println("Booking not added.");
-            }
+            }  */
+
+            // test. GET BOOKING BY ID
+            int testing_id = 12;
+            Booking booking = bookingDao.getBookingById(testing_id);
+            int seat = booking.getSeat();
+            System.out.println("Booking ID: " + testing_id + " has seat number: " + seat);
         } catch (SQLException e) {
             System.out.println("Database connection failed.");
             System.out.println(e.getMessage());
