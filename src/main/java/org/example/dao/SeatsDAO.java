2ae1c0dc0ed2c14be4ee9a9e6d7fd7b84661198d
Abdullah Hared <muxsinxareed6@gmail.com>
2025-01-21 10:47:16 +0100

    Created a method to insert customer seat to the db

--- src/main/java/org/example/dao/SeatsDAO.java
+++ src/main/java/org/example/dao/SeatsDAO.java
@@ -6,4 +6,22 @@ import java.util.List;
 
 public class SeatsDAO {
 
+    private final Connection connection;
+
+    public SeatsDAO(Connection connection) {
+        this.connection = connection;
+    }
+
+    // add a seat
+    public void addSeat(String seatNumber) {
+        String query = "INSERT INTO booked_seats (seat_number) VALUES (?)";
+        try (PreparedStatement ps = connection.prepareStatement(query)) {
+            ps.setString(1, seatNumber);
+            ps.executeUpdate();
+            System.out.println("Successfully added seat: "  + seatNumber);
+        } catch (SQLException e) {
+            System.out.println("Error adding seat to db" + e.getMessage());
+        }
+    }
+
 }
