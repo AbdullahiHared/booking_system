f04eb4753d068d444b41b62d4b756af1e11c7618
Abdullah Hared <muxsinxareed6@gmail.com>
2025-01-26 11:45:03 +0100

    Return the customer id correctly

--- src/main/java/org/example/model/Booking.java
+++ src/main/java/org/example/model/Booking.java
@@ -1,14 +1,14 @@
 package org.example.model;
 
-import java.util.Date;
-
 public class Booking {
     private int id;
     private Customer customer;
+    private int seat;
 
     // Constructors
-    public Booking(int id, Customer customer) {
+    public Booking(int id, int seat, Customer customer) {
         this.id = id;
+        this.seat = seat;
         this.customer = customer;
     }
 
@@ -19,15 +19,34 @@ public class Booking {
         return id;
     }
 
-    public int getSeat() {
-        return customer.getCustomerSeat();
-    }
-
     public void setId(int id) {
         this.id = id;
     }
 
+    public int getSeat() {
+        return seat;
+    }
+
+    public void setSeat(int seat) {
+        this.seat = seat;
+    }
+
     public Customer getCustomer() {
         return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public int getCustomerId() {
+        return customer != null ? customer.getId() : -1; // Return -1 if customer is null
+    }
+
+    public void setCustomerId(int customerId) {
+        if (this.customer == null) {
+            this.customer = new Customer();
+        }
+        this.customer.setId(customerId);
     }
 }
