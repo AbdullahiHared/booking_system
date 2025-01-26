7650803209fc8ba7423566ec72f75ad26c12138d
Abdullah Hared <muxsinxareed6@gmail.com>
2025-01-26 20:22:22 +0100

    feat: Initialize DAOs and BookingService in Main class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,34 @@
 package org.example;
 
+import org.example.dao.BookingDAO;
+import org.example.dao.CustomerDAO;
+import org.example.dao.SeatsDAO;
+import org.example.database.DatabaseConnection;
+import org.example.model.Customer;
+import org.example.service.BookingService;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class Main {
+    private static BookingService bookingService;
+    private static Customer currentCustomer;
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = scanner = new Scanner(System.in);
+        try (Connection connection = DatabaseConnection.getConnection()) {
+            BookingDAO bookingDAO = new BookingDAO(connection);
+            CustomerDAO customerDAO = new CustomerDAO(connection);
+            SeatsDAO seatsDAO = new SeatsDAO(connection);
+            bookingService = new BookingService(bookingDAO, customerDAO, seatsDAO);
+
+
+            // Main menu loop here
+        } catch (SQLException e) {
+            System.err.println("Error connecting to the database: " + e.getMessage());
+        } finally {
+            scanner.close();
+        }
     }
 }
