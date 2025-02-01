e7b79a8e09d3b9dacc53f11f3246f4f707b4857c
Abdullah Hared <muxsinxareed6@gmail.com>
2025-02-01 09:06:25 +0100

    Initiate businspector dao

--- src/main/java/org/example/dao/BusInspectorDAO.java
+++ src/main/java/org/example/dao/BusInspectorDAO.java
@@ -1,4 +1,11 @@
 package org.example.dao;
 
+import org.example.database.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class BusInspectorDAO {
 }
