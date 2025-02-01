0badd0868c1ad68d003c5312fc1d40b0c1030943
Abdullah Hared <muxsinxareed6@gmail.com>
2025-02-01 09:11:31 +0100

    Add: method to store profit data

--- src/main/java/org/example/dao/BusInspectorDAO.java
+++ src/main/java/org/example/dao/BusInspectorDAO.java
@@ -2,10 +2,24 @@ package org.example.dao;
 
 import org.example.database.DatabaseConnection;
 
+import javax.xml.crypto.Data;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class BusInspectorDAO {
+
+    // method to store profit data
+    public void addProfit(double amount) {
+        String query = "INSERT INTO profits (amount) VALUES(?)";
+        try (Connection connection = DatabaseConnection.getConnection()) {
+            PreparedStatement ps = connection.prepareStatement(query);
+            ps.setDouble(1, amount);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Error: "  + e.getMessage());
+        }
+    }
+
 }
